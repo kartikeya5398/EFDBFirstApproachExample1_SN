7134a266defcc65e66174aab824624eb06fa9c4e
Kumar Kartikeya <kartikeya.kumar@definelabs.in>
2022-09-15 01:25:11 +0530

    Add project files.

---
+++ EFDBFirstApproachExample1/Controllers/BrandsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFDBFirstApproachExample1.Models;
+
+namespace EFDBFirstApproachExample1.Controllers
+{
+    public class BrandsController : Controller
+    {
+        // GET: Brands
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            List<Brand> brands = db.Brands.ToList();
+            return View(brands);
+        }
+    }
+}
