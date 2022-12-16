258c18d6384158f72233f9707b329d868575e038
Kumar Kartikeya <kartikeya.kumar@definelabs.in>
2022-12-17 03:33:33 +0530

    FilterOverrides Lec-100Completed

---
+++ EFDBFirstApproachExample1/App_Start/FilterConfig.cs
@@ -0,0 +1,17 @@
+﻿using EFDBFirstApproachExample1.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFDBFirstApproachExample1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilter(GlobalFilterCollection filters)
+        {
+            filters.Add(new MyExceptionFilter());
+        }
+    }
+}
---
+++ EFDBFirstApproachExample1/Filter/MyExceptionFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace EFDBFirstApproachExample1.Filter
+{
+    public class MyExceptionFilter : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            string msg = "Message :" + filterContext.Exception.Message + "Type :" + filterContext.Exception.GetType().ToString() + "Source :" + filterContext.Exception.Source;
+            StreamWriter sw = File.AppendText(filterContext.RequestContext.HttpContext.Request.PhysicalApplicationPath + "\\ErrorLog.txt");
+            sw.WriteLine(msg);
+            sw.Close();
+            filterContext.ExceptionHandled = true;
+            filterContext.Result = new RedirectResult("~/Error.html");
+        }
+    }
+}
